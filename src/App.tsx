160c505fe3957ff27b3f166628a97b9c70cd8703
WillyMay <willian.mayer@proton.me>
2024-07-02 14:11:48 -0400

    fetching news

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,6 @@
 import News from "./components/main/News";
 
 function App() {
-  const url =
-    "https://newsapi.org/v2/everything?q=keyword&apiKey=24897e7e79cf47d79a805ccd483ab2d4";
-
-  fetch(url, {
-    method: "GET",
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then((data) => console.log(data))
-    .catch((error) =>
-      console.error("There was a problem with the fetch operation:", error)
-    );
-  return <div>News</div>;
   return (
     <>
       <h1 className="text-3xl font-bold">Hello World!</h1>
--- src/components/main/News.tsx
+++ src/components/main/News.tsx
@@ -1,7 +1,21 @@
 function News() {
-  return (
-    <div>News</div>
-  )
+  const url =
+    "https://newsapi.org/v2/everything?q=keyword&apiKey=24897e7e79cf47d79a805ccd483ab2d4";
+
+  fetch(url, {
+    method: "GET",
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => console.log(data))
+    .catch((error) =>
+      console.error("There was a problem with the fetch operation:", error)
+    );
+  return <div>News</div>;
 }
 
-export default News
+export default News;
